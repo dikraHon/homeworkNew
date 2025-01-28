cd002a0e7c0e98b8cd3044c48878d0d2477c2ce6
Dikra <Dikramfvdf>
2025-01-28 15:57:48 +0300

    add func createMassiv

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    }
+    public static int[] createMassive(int sizeMas){
+            int[] mas = new int[sizeMas];
+            int i = 0;
+            while (i < sizeMas){
+                mas[i] = new Random().nextInt(-5,5);
+                ++i;
+            }
+            return mas;
+    }
+    /*
+     Задание 1: Сортировка массива с отрицательными числами:
+ Отсортируйте массив, чтобы отрицательные числа находились перед
+положительными, *сохраняя порядок внутри каждой группы.
+ Пример:
+ Вход: [4, -3, 2, -1, 0, -5].
+ Выход: [-3, -1, -5, 4, 2, 0].
+    */
+
+    public static void sortArray(int[] mas){
+
     }
 }
