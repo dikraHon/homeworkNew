1c62338e3aa83cf4b5c79fa25544fb539c3853c1
Dikra <Dikramfvdf>
2025-02-11 18:55:09 +0300

    solution1

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,52 @@
 public class Main {
     public static void main(String[] args) {
-        Product electronics = new Electronics();
+        Electronics electronics = new Electronics();
         Clothing clothing = new Clothing();
         Food food = new Food();
-
-       System.out.println("electronics: " + electronics.getFinalPrice(90));
-       System.out.println("closing: " + clothing.getFinalPrice(90));
-       System.out.println("food: " + food.getFinalPrice(90));
+        electronics.price = 90;
+        clothing.price = 90;
+        food.price = 90;
+        System.out.println("electronics: " + electronics.getFinalPrice());
+        System.out.println("closing: " + clothing.getFinalPrice());
+        System.out.println("food: " + food.getFinalPrice());
     }
 }
 
 interface Product {
-    double getFinalPrice(double price);
+    double getFinalPrice();
 }
 
-class Electronics implements Product {
-    public double getFinalPrice(double price) {
+class Price{
+    public double price;
+    public double price(double price) {
+        this.price = price;
+        return price;
+    }
+}
+
+class Electronics extends Price implements Product {
+    public double price(double price) {
+        return super.price(price);
+    }
+    public double getFinalPrice() {
         return price - (price * 0.1);
     }
 }
 
-class Clothing implements Product {
-    public double getFinalPrice(double price) {
+class Clothing extends Price implements Product {
+    public double price(double price) {
+        return super.price(price);
+    }
+    public double getFinalPrice() {
         return price - (price * 0.15);
     }
 }
 
-class Food implements Product {
-    public double getFinalPrice(double price) {
+class Food extends Price implements Product {
+    public double price(double price) {
+        return super.price(price);
+    }
+    public double getFinalPrice() {
         return price;
     }
 }
